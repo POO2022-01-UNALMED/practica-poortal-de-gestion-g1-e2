146a4699a3a8ffae9d69ce9a08f1267bd6ae2a4b
Alejandro Alvarez <92798860+Alejoab@users.noreply.github.com>
2022-05-02 21:22:28 -0500

    Añade clases Inventario y Producto

---
+++ src/gestorAplicacion/Inventario.java
@@ -0,0 +1,82 @@
+package gestorAplicacion;
+
+import java.util.ArrayList;
+
+public class Inventario {
+	private static ArrayList<Producto> listadoProductos = new ArrayList<Producto>();
+	private static ArrayList<Servicio> listadoServicios = new ArrayList<Servicio>();
+	private static ArrayList<Factura> listadoFacturas = new ArrayList<Factura>();
+	private static ArrayList<Empleado> listadoEmpleados = new ArrayList<Empleado>();
+	
+
+	public static ArrayList<Producto> getListadoProductos() {
+		return listadoProductos;
+	}
+
+
+	public static ArrayList<Servicio> getListadoServicios() {
+		return listadoServicio;
+	}
+
+
+	public static ArrayList<Factura> getListadoFacturas() {
+		return listadoFacturas;
+	}
+
+
+	public static ArrayList<Empleado> getListadoEmpleados() {
+		return listadoEmpleado;
+	}
+	
+	
+	public static void agregarProducto(Producto producto) {
+		listadoProductos.add(producto);
+	}
+	
+
+	public static void agregarServicio(Servicio servicio) {
+		listadoServicios.add(servicio);
+	}
+	
+	public static void agregarFactura(Factura factura) {
+		listadoFacturas.add(factura);
+	}
+	
+	public static void agregarEmpleado(Empleado empleado) {
+		listadoEmpleados.add(empleado);
+	}
+	
+	public static Producto buscarProducto(String nombre) {
+		for (Producto i:listadoProductos) {
+			if (i.getNombre().equals(nombre)) {
+				return i;
+			}
+		}
+	}
+	
+	public static Servicio buscarServicio(String nombre) {
+		for (Servicio i:listadoServicios) {
+			if (i.getNombre().equals(nombre)) {
+				return i;
+			}
+		}
+	}
+	
+	public static Empleado buscarEmpleado(String nombre) {
+		for (Empleado i:listadoEmpleados) {
+			if (i.getNombre().equals(nombre)) {
+				return i;
+			}
+		}
+	}
+	
+	public static Factura buscarFactura(int num) {
+		for (Factura i:listadoFacturas) {
+			if (i.getConsecutivo() == num) {
+				return i;
+			}
+		}
+	}
+	
+		
+}
---
+++ src/gestorAplicacion/Producto.java
@@ -0,0 +1,86 @@
+package gestorAplicacion;
+
+
+enum Categoria{
+	aseo, ropa, alimentos, mascotas, salud, tecnologia;
+}
+
+
+public class Producto {
+	private String nombre;
+	private int cantidadDisponible;
+	private Categoria categoria;
+	private int precio;
+	private int mesesGarantia;
+	
+	
+	public Producto(String nombre, int cantidadDisponible, Categoria categoria, int precio, int mesesGarantia) {
+		this.nombre = nombre;
+		this.cantidadDisponible = cantidadDisponible;
+		this.categoria = categoria;
+		this.precio = precio;
+		this.mesesGarantia = mesesGarantia;
+		Inventario.agregarProducto(this);
+	}
+	
+	public Producto(String nombre, Categoria categoria, int garantia) {
+		this(nombre, 0, categoria, 0, garantia);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public int getMesesGarantia() {
+		return mesesGarantia;
+	}
+
+	public void setMesesGarantia(int mesesGarantia) {
+		this.mesesGarantia = mesesGarantia;
+	}
+
+	public int getCantidadDisponible() {
+		return cantidadDisponible;
+	}
+	
+	
+	public void reabastecer(int num) {
+		cantidadDisponible += num;
+	}
+	
+	public boolean verificarCantidad(int num) {
+		if (cantidadDisponible >= num) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void vender(int num) {
+		if (verificarCantidad(num)) {
+			cantidadDisponible -= num;
+		}
+	}
+	
+}
