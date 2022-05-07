620dfec022be99a436e6225b9e8cfd12898354d9
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-05-06 21:42:11 -0500

    creación clase Empleado

---
+++ src/gestorAplicacion/Empleado.java
@@ -0,0 +1,12 @@
+package gestorAplicacion;
+
+public class Empleado extends Persona{
+    private Contrato contrato;
+    private String cargo;
+    private String departamento;
+    private Servicio servicio;
+
+    public Empleado(){
+        
+    }
+}
