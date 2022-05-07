fa8102e7fdf997419c72b7012e4e28eae5d8247e
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-05-06 22:02:36 -0500

    clase empleado

--- src/gestorAplicacion/Empleado.java
+++ src/gestorAplicacion/Empleado.java
@@ -6,7 +6,39 @@ public class Empleado extends Persona{
     private String departamento;
     private Servicio servicio;
 
-    public Empleado(){
-        
+    public Empleado(Contrato contrato, String cargo, String departamento, Servicio servicio){
+        super(nombre, telefono, email, identificacion, tipoDeIdentificion, sexo);
+        this.contrato = contrato;
+        this.cargo = cargo;
+        this.departamento = departamento;
+        this.servicio = servicio;
     }
+
+    public Contrato getContrato() {
+		return contrato;
+	}
+	public void setContrato(Contrato contrato) {
+		this.contrato = contrato;
+	}
+
+    public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+    public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+    public Servicio getServicio() {
+		return servicio;
+	}
+	public void setServicio(Servicio servicio) {
+		this.servicio = servicio;
+	}
 }
