597aa5d6a6b8e22f8a5e8f4721fa50aa76092d51
Alejandro Alvarez <92798860+Alejoab@users.noreply.github.com>
2022-05-16 15:09:02 -0500

    Añade funcionalidades a la interfaz

--- src/uiMain/Interfaz.java
+++ src/uiMain/Interfaz.java
@@ -1,11 +1,46 @@
 package uiMain;
 
+import java.util.Scanner;
+
 import gestorAplicacion.*;
 
 public class Interfaz {
+	static Scanner sc = new Scanner(System.in);
+	
+	static int readInt() {
+		return sc.nextInt();
+	}
+	
+	static Double readDouble() {
+		return sc.nextDouble();
+	}
+	
+	static String readString() {
+		return sc.nextLine();
+	}
 	
 	public static void main(String[] args) {
+		System.out.println("Buenos días Administrador\n\n");
 		
+		int opcion;
+		
+		do {
+			System.out.println("Selecione que accion quiere realizar\n");
+			
+			System.out.println("Gestionar Empleados");
+			System.out.println("Gestionar Carrito");
+			System.out.println("Devolver Producto");
+			System.out.println("Elegir empleados para servicios seleccionados");
+			System.out.println("Pagar");
+			System.out.println("Guardar y cerrar");
+			
+			opcion = readInt();
+			
+			switch (opcion) {
+			
+			}
+			
+		} while (opcion != 6);
 	}
 	
 }
