03c3e1b379a3ac17adacac6320e95fddbe8b15bb
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-05-23 19:23:46 -0500

    Clase enum para categorias de productos agregada

---
+++ src/gestorAplicacion/Categoria.java
@@ -0,0 +1,12 @@
+package gestorAplicacion;
+
+public enum Categoria {
+	ASEO("Aseo"), ROPA("Ropa"), ALIMENTACION("Alimentacion"),
+    MASCOTA("Mascota"), SALUD("Salud"), TECNOLOGIA("Tecnologia");
+
+	public final String label;
+
+	Categoria(String label) {
+		this.label = label;
+	}
+}
