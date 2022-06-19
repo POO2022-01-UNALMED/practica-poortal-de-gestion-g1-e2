fcac6d2441fa1375fa9b4b98baf7bba3021ae124
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2022-06-19 03:12:12 -0500

    Añade clases Cliente e Inventario

---
+++ python/src/gestorAplicacion/general/Inventario.py
@@ -0,0 +1,184 @@
+from personas import Empleado
+from personas import Cliente
+
+
+class Inventario:
+
+    #private static final long serialVersionUID = 1L
+
+    listadoProductos = []
+    listadoServicios = []
+    listadoFacturas = []
+    listadoPersonas = []
+
+    @classmethod
+    def getListadoProductos(cls):
+        return cls.listadoProductos
+    
+
+    @classmethod
+    def getListadoServicios(cls):
+        return cls.listadoServicios
+    
+
+    # Los servicios disponibles son aquellos en los que se hay al menos un empleado
+    # trabajando en el
+    @classmethod
+    def getServiciosDisponibles(cls):
+        serviciosDisp = []
+
+        # De cada empleado se extrae su servicio y se agrega a la lista
+        for i in cls.getListadoEmpleados():
+            if i.getServicio() == None:
+                serviciosDisp.append(i.getServicio())
+            
+        # Se eliminan los servicios repetidos
+        return set(serviciosDisp)
+    
+
+    @classmethod
+    def getListadoFacturas(cls):
+        return cls.listadoFacturas
+    
+
+    @classmethod
+    def getListadoEmpleados(cls):
+        empleados = []
+
+        for i in cls.listadoPersonas:
+            if isinstance(i, Empleado):
+                empleados.append(i)
+            
+        return empleados
+    
+    
+    @classmethod
+    def getListadoEmpleadosActivos(cls):
+        empleadosActivos = []
+        for i in cls.getListadoEmpleados():
+            if i.isActivo():
+                empleadosActivos.append(i)
+            
+        return empleadosActivos
+    
+
+    @classmethod
+    def agregarProducto(cls, producto):
+        cls.listadoProductos.append(producto)
+    
+
+    @classmethod
+    def agregarServicio(cls, servicio):
+        cls.listadoServicios.append(servicio)
+    
+
+    @classmethod
+    def agregarFactura(cls, factura):
+        cls.listadoFacturas.append(factura)
+    
+
+    @classmethod
+    def agregarPersona(cls, persona):
+        cls.listadoPersonas.append(persona)
+
+    
+    @classmethod
+    def eliminarPersona(cls, persona):
+        cls.listadoPersonas.remove(persona)
+    
+
+    @classmethod
+    def getListadoPersonas(cls):
+        return cls.listadoPersonas
+    
+
+    @classmethod
+    def buscarProducto(cls, nombre):
+        for i in cls.listadoProductos:
+            if i.getNombre() == nombre:
+                return i
+            
+        
+        raise ValueError("No hay Productos que coincidan con el nombre especificado")
+    
+
+    @classmethod
+    def buscarServicio(cls, nombre):
+        for i in cls.listadoServicios:
+            if i.getNombre().equals(nombre):
+                return i
+            
+        raise ValueError("No hay Servicios que coincidan con el nombre especificado")
+    
+
+    @classmethod
+    def buscarEmpleado(cls, nombre):
+        for i in cls.getListadoEmpleados():
+            if (i.getNombre().equals(nombre)):
+                return i
+            
+        raise ValueError("No hay Empleados que coincidan con el nombre especificado")
+    
+
+    @classmethod
+    def buscarFactura(cls, **kwargs):
+        if kwargs.num:
+            for i in cls.listadoFacturas:
+                if i.getConsecutivo() == kwargs.num:
+                    return i
+                
+            raise ValueError("\nNo hay Facturas que coincidan con el consecutivo especificado\n\n")
+        elif kwargs.date:
+            for  i in cls.listadoFacturas:
+                if i.getFechaExpedicion() == kwargs.date:
+                    return i
+                
+            raise ValueError("No hay Facturas que coincidan con la fecha especificada")
+        else:
+            raise ValueError("Se requiere una fecha o consecutivo para buscar facturas")
+            
+
+    @classmethod
+    def clientesConCarrito(cls):
+        clientes = []
+
+        for i in cls.listadoPersonas:
+            if isinstance(i, Cliente) and not i.carritoVacio():
+                clientes.append(i)
+            
+        
+        if not len(clientes):
+            raise ValueError("\nNo hay clientes con servicios o productos en su carrito de compra\n\n")
+        
+        return clientes
+    
+
+    @classmethod
+    def getClientes(cls):
+        clientes = []
+        for i in cls.listadoPersonas:
+            if isinstance(i, Cliente):
+                clientes.append(i)
+            
+        if not len(clientes):
+            raise ValueError("\nNo hay clientes disponibles en este momento\n\n")
+        
+        return clientes
+    
+
+    @classmethod
+    def getProductosDisponibles(cls):
+        productosDisp = []
+
+        # Solo se van a retornar los productos que tienen una cantidad disponible
+        # superior a 0
+        for i in cls.listadoProductos:
+            if i.getCantidappendisponible() > 0:
+                productosDisp.append(i)
+            
+        if not len(productosDisp):
+            raise ValueError("\nNo tenemos productos disponibles en este momento\n\n")
+        
+        return productosDisp
+    
+
---
+++ python/src/gestorAplicacion/personas/Cliente.py
@@ -0,0 +1,234 @@
+from general import Inventario
+from ventas import Factura, Producto, Servicio
+
+from personas import Persona
+from dateutil.relativedelta import relativedelta
+
+# Esta clase extiende de persona y se encarga de definir
+# todos los atributos y metodos necesarios para gestionar su carrito 
+# y realizar compras y devoluciones
+# 
+# @author Mateo Alvarez Lebrum
+# @author Alejandro Alvarez Botero
+# @author Miguel Angel Barrera Bustamante
+# @author Alejandra Barrientos Grisales
+
+#TODO actualizar error segun las condiciones del trabajo
+#TODO ajustar documentacion de metodos
+class Cliente(Persona):
+    #private static final long serialVersionUID = 1L
+
+    def __init__(self, nombre, telefono, email, identificacion, tipoDeIdentificacion, sexo):
+        super().__init__(nombre, telefono, email, identificacion, tipoDeIdentificacion, sexo)
+     
+
+    # Este metodo genera una factura con los productos que hay en el carrito
+    # @return Factura de compra
+    def pagar(self):
+        # Verifica que todos los servicios del carrito tengan empleado asignado
+        if None in self._servicios.values():
+            raise ValueError("\nActualmente tiene servicios sin empleado asignado, por favor seleccione empleados primero.\n\n")
+        
+
+        # Se genera una factura asociada a la compra
+        factura = Factura(self._productos, self._servicios, self.identificacion)
+
+        # Por cada producto del carrito se ejecuta su metodo "vender" que disminuye su cantidad disponible en la tienda
+        for producto in self._productos:
+            producto.vender(self._productos[producto])
+        
+
+        # Se limpia el carrito de compras
+        self._productos = {}
+        self._servicios = {}
+
+        return factura
+    
+
+    #  Este metodo busca dentro de los servicios del carrito cuales no tienen un empleado asignado
+    #  @return lista de servicios sin empleados
+    def obtenerServiciosSinEmpleado(self):
+        serviciosSinEmpleado = []
+        
+        # Recorre el hashmap correspondiente a servicio y ve cuales no tienen un objeto empleado como value
+        for servicio in self._servicios:
+            if self._servicios[servicio] == None:
+                serviciosSinEmpleado.append(servicio)
+
+        if len(serviciosSinEmpleado) == 0:
+            raise ValueError("\nEl cliente no tiene servicios a los cuales les deba asignar un empelado\n\n")
+        
+        return serviciosSinEmpleado
+    
+
+    # @param producto
+    # @param cantidad
+    def agregarProductoALaCanasta(self, producto, cantidad):
+        # Si el producto ya esta en el carrito de compras le suma la nueva cantidad
+        # ingresada a la cantidad que ya tenia
+        for i in self._productos:
+            if i == producto:
+                # Actualiza la cantidad del producto en los diferentes carritos
+                producto.agregarCantidadCarrito(cantidad)
+
+                self._productos[i] = self._productos[i] + cantidad
+                return
+
+        # Si el producto no esta en el carrito lo agrega junto a su cantidad
+        producto.agregarCantidadCarrito(cantidad)
+        self._productos[producto] = cantidad 
+
+
+     # @param producto
+    def eliminarProductoDeLaCanasta(self, producto):
+        # Actualiza la suma de las cantidades del productos en los diferentes carritos
+        producto.disminuirCantidadCarrito(self._productos[producto])
+        self._productos.pop(producto)
+    
+
+    # @param servicio
+    def solicitarServicio(self, servicio):
+        # Verifica que el servicio no se encuentre en el carrito
+        if (servicio in self._servicios.keys()):
+            raise ValueError("El servicio ya fue solicitado.")
+        
+        self._servicios[servicio] = None
+    
+
+    # Este metodo asigna un empleado a un servicio en el carrito
+    # @param servicio
+    # @param empleado
+    def seleccionarEmpleado(self, servicio, empleado):
+        for  i in self._servicios:
+            if i == servicio:
+                self._servicios[i] = empleado
+
+    # @param servicio
+    def eliminarServicioDeLaCanasta(self, servicio):
+        self._servicios.pop(servicio)
+    
+
+    # @param nombreProducto
+    # @param identificacion
+    # @param cantidadADevolver
+    # @param fecha
+    # @return Mensaje con la informacion del dinero retornado al cliente
+    @staticmethod
+    def devolverProducto(nombreProducto, identificacion, cantidadADevolver, fecha):
+        
+        # Verificar que existe un producto con ese nombre en el inventario
+        productoEncontrado = False
+        productoComprado = None
+        for producto in Inventario.getListadoProductos():
+            if producto.getNombre() == nombreProducto:
+                productoEncontrado = True
+                productoComprado = producto
+
+
+        if not productoEncontrado:
+            raise ValueError("El producto no existe en nuestro inventario")
+
+        # Verificar que existe una factura en ese dia con ese producto
+
+        facturaEncontrada = False
+        facturaCompra = None
+        fechaProporcionada = fecha
+        
+        # Encuentra factura que contiene ese producto y fue comprado por la persona que
+        # lo esta devolviendo y coincide en la fecha proporcionada
+        for factura in Inventario.getListadoFacturas():
+            if factura.getNumeroIdentificacionPersona() == identificacion and Inventario.buscarFactura(fechaProporcionada) != None:
+                for producto in factura.getProductos():
+                    if productoComprado == producto:
+                        facturaEncontrada = True
+                        facturaCompra = factura
+
+
+        if not facturaEncontrada:
+            raise ValueError("No existen facturas con dicha informacion de compra asociada(producto/identificacion/fecha)")
+
+        # Verificar que los productos a devolver sean menores a los comprados
+
+        cantidadValida = False
+        for compra in facturaCompra.getProductos():
+            if compra == productoComprado:
+                if facturaCompra.getProductos()[compra] >= cantidadADevolver:
+                    cantidadValida = True
+
+
+        if not cantidadValida:
+            raise ValueError("Intenta devolver mas productos de los que fueron comprados")
+
+        # Verificar que el tiempo de garantia aun se cumpla
+        tiempoMaximo = facturaCompra.getFechaExpedicion() + relativedelta(months=productoComprado.getMesesGarantia())
+        if tiempoMaximo < fechaProporcionada:
+            raise ValueError("Ya paso el tiempo de garantia")
+
+        # Modificar el total de la factura y la cantidad de productos
+
+        reembolso = productoComprado.getPrecio() * cantidadADevolver
+        facturaCompra.reajustarTotal(facturaCompra.getTotal() - reembolso)
+        facturaCompra.retirarProducto(productoComprado, cantidadADevolver)
+
+        # Retorna dinero de reembolso
+        texto = "\nLa devolucion se realizo exitosamente\n" + "\nSe han devuelto " + cantidadADevolver + " "
+        + nombreProducto + "(s) del cliente con identificacion: " 
+        + facturaCompra.getNumeroIdentificacionPersona() + "\n" + "\nEl dinero retornado es " + reembolso 
+        + "\n"
+
+        return texto
+    
+
+    def carritoVacio(self):
+        if len(self._productos) or len(self._servicios):
+            return False
+        return True
+
+    # Muestra la informacion del cliente
+    def mostrarInformacion(self):
+        return "Soy el cliente " + self._nombre + " con numero de identificacion: " + self._identificacion
+    
+    
+    # @return String con la informacion de los productos y servicios en el carrito
+    def verCarrito(self):
+        text = ""
+
+        # Si el hashmap de productos tiene algun elemento, se agrega la informacion de
+        # la cantidad disponible y el nombre, en caso contrario no se hace nada
+        if len(self._productos):
+            for i in self._productos:
+                text += self._productos[i] + " unidad(es) del producto " + i.getNombre() + "\n"
+
+
+        # Si el hashmap de servicios tiene algun elemento, se agrega l
+        if len(self._servicios):
+            for i in self.servicios:
+                if self.servicios[i] == None:
+                    text += "Servicio " + i.getNombre() + " que no tiene un empleado asignado aun\n"
+                else:
+                    text += "Servicio " + i.getNombre() + " que sera ejecutado por el empleado "
+                    + self.servicios[i].getNombre() + "\n"
+                    
+
+        # Si no tiene ningun producto o servicio se retorna el siguiente mensaje
+        if text == "":
+            return "No tiene productos ni servicios en su carrito"
+        
+        return text
+    
+
+    def getServicios(self):
+        if not len(self._servicios):
+            raise ValueError("No tiene servicios en el carrito actualmente")
+        
+        return self._servicios.keys()
+    
+
+    def getProductos(self):
+        if not len(self._productos):
+            raise ValueError("No tiene productos en el carrito actualmente")
+        
+        return self._productos.keys()
+    
+    
+
